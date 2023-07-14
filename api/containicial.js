be395f72943f2827b73b5aeb4cd3267d1a3e92b8
BuildTools <unconfigured@null.spigotmc.org>
2023-07-13 22:58:07 -0300

    beta1

--- api/containicial.js
+++ api/containicial.js
@@ -20,7 +20,7 @@ router.use((req, res, next) => {
     router.use(cors());
     next();
 });
-const maxAttempts = 3;
+const maxAttempts = 8;
 const windowMs = 30 * 60 * 1000;
 const limiter = new RateLimiterMemory({
   points: maxAttempts,
@@ -42,69 +42,69 @@ router.post('/cadastrar', [body('form.nome').trim().isLength({ min: 3 }).withMes
         const clientIp = req.ip
         const rateLimiterRes = await limiter.consume(clientIp);
         const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ msg: errors.errors[0].msg });
-    }
-    const sanitizedData = {
-        form: {
-          nome: DOMPurify.sanitize(req.body.form.nome),
-          email: DOMPurify.sanitize(req.body.form.email),
-          senha: DOMPurify.sanitize(req.body.form.senha)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ msg: errors.errors[0].msg });
         }
-    };
-    console.log(sanitizedData)
-    const validarUsuario = await UserConfig.findOne({
-        where: {
-            email: sanitizedData.form.email,
-        }
-    });
-    if(!validarUsuario){
-        sanitizedData.form.senha = await bcrypt.hash(sanitizedData.form.senha, 8)
-        sanitizedData.form.endereco = await req.ip
-        sanitizedData.form.stats = '4'
-        let entradasSalvarCadastro = 0
-        let entradasInfo = 0
-        var novoUsuarioId
-        while(entradasSalvarCadastro < 3){
-            try{
-                const novoUsuario = await UserConfig.create(sanitizedData.form)
-                novoUsuarioId = novoUsuario.id
-                break;
-            }catch(error){
-                entradasSalvarCadastro++
+        const sanitizedData = {
+            form: {
+                nome: DOMPurify.sanitize(req.body.form.nome),
+                email: DOMPurify.sanitize(req.body.form.email),
+                senha: DOMPurify.sanitize(req.body.form.senha)
             }
-        }
-        while(entradasInfo < 3){
-            try{
-                const novoUsuarioInfo = await UserInfo.create({id_user: novoUsuarioId})
-                break;
-            }catch(error){
-                entradasSalvarCadastro++
+        };
+        console.log(sanitizedData)
+        const validarUsuario = await UserConfig.findOne({
+            where: {
+                email: sanitizedData.form.email,
             }
-        }
-        if(entradasInfo === 3){
-            return res.json({
-                status: `1_CB#15560`,
-                msg: 'Conseguimos criar o seu login, porem não conseguimos criar as suas informações, entre em contato com o suporte =)',
-            });
-        }
-        if(entradasSalvarCadastro === 3){
-            return res.json({
-                status: '1_CB#28042',
-                msg: 'Não foi possível criar a tabela após ' + entradasSalvarCadastro + ' tentativas. Tente realizar o cadastro novamente',
-            });
-        }
-        EnviarEmailInicial(sanitizedData.form.email,  sanitizedData.form.nome, '0000', 0)
-        console.log(`Usuario ${sanitizedData.form.email} Cadastrado`)
-        return res.json({
-            status: ``,
-            msg: `sucesso`,
         });
-    }
-    return res.json({
-        status: `1_CB#09411`,
-        msg: `erro, o email já está cadastrado`,
-    });
+        if(!validarUsuario){
+            sanitizedData.form.senha = await bcrypt.hash(sanitizedData.form.senha, 8)
+            sanitizedData.form.endereco = await req.ip
+            sanitizedData.form.stats = '4'
+            let entradasSalvarCadastro = 0
+            let entradasInfo = 0
+            var novoUsuarioId
+            while(entradasSalvarCadastro < 3){
+                try{
+                    const novoUsuario = await UserConfig.create(sanitizedData.form)
+                    novoUsuarioId = novoUsuario.id
+                    break;
+                }catch(error){
+                    entradasSalvarCadastro++
+                }
+            }
+            while(entradasInfo < 3){
+                try{
+                    const novoUsuarioInfo = await UserInfo.create({id_user: novoUsuarioId})
+                    break;
+                }catch(error){
+                    entradasSalvarCadastro++
+                }
+            }
+            if(entradasInfo === 3){
+                return res.json({
+                    status: `1_CB#15560`,
+                    msg: 'Conseguimos criar o seu login, porem não conseguimos criar as suas informações, entre em contato com o suporte =)',
+                });
+            }
+            if(entradasSalvarCadastro === 3){
+                return res.json({
+                    status: '1_CB#28042',
+                    msg: 'Não foi possível criar a tabela após ' + entradasSalvarCadastro + ' tentativas. Tente realizar o cadastro novamente',
+                });
+            }
+            EnviarEmailInicial(sanitizedData.form.email,  sanitizedData.form.nome, '0000', 0)
+            console.log(`Usuario ${sanitizedData.form.email} Cadastrado`)
+            return res.json({
+                status: ``,
+                msg: `sucesso`,
+            });
+        }
+        return res.json({
+            status: `1_CB#09411`,
+            msg: `erro, o email já está cadastrado`,
+        });
     }catch (error) {
         return res.status(429).json({
           msg: 'Você tentou realizar esta ação muitas vezes. Aguarde alguns minutos e tente novamente.',
@@ -117,89 +117,150 @@ router.post('/logar', [body('form.email').trim().isEmail().withMessage('Email in
         const clientIp = req.ip
         const rateLimiterRes = await limiter.consume(clientIp);
         const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ msg: errors.errors[0].msg });
-    }
-    const sanitizedData = {
-        form: {
-          email: DOMPurify.sanitize(req.body.form.email),
-          senha: DOMPurify.sanitize(req.body.form.senha)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ msg: errors.errors[0].msg });
         }
-    };
-    const validarUsuario = await UserConfig.findOne({
-        where: {
-            email: sanitizedData.form.email,
-        }
-    });
-    if(validarUsuario){
-        if((await bcrypt.compare(sanitizedData.form.senha, validarUsuario.senha))){
-            const diferencaData = (validarUsuario.updatedAt - new Date()) / (1000 * 60 * 60 * 24)*(-1)
-            if((validarUsuario.stats[0] == '3' || validarUsuario.stats[0] == '4') || (validarUsuario.endereco != req.ip && (diferencaData < 1 || diferencaData > 30))){
-                console.log(diferencaData)
-                console.log(`validação do email do ${sanitizedData.form.email}`)
-                var newCodigo
-                if(validarUsuario.stats[0] == '4'){
-                    newCodigo = Codigo(4, validarUsuario.id)
-                }else{
-                    newCodigo = Codigo(3, validarUsuario.id)
+        const sanitizedData = {
+            form: {
+                email: DOMPurify.sanitize(req.body.form.email),
+                senha: DOMPurify.sanitize(req.body.form.senha)
+            }
+        };
+        const validarUsuario = await UserConfig.findOne({
+            where: {
+                email: sanitizedData.form.email,
+            }
+        });
+        if(validarUsuario){
+            if((await bcrypt.compare(sanitizedData.form.senha, validarUsuario.senha))){
+                const diferencaData = (validarUsuario.updatedAt - new Date()) / (1000 * 60 * 60 * 24)*(-1)
+                if((validarUsuario.stats[0] == '3' || validarUsuario.stats[0] == '4') || (validarUsuario.endereco != req.ip && (diferencaData < 1 || diferencaData > 30))){
+                    console.log(`validação do email do ${sanitizedData.form.email}`)
+                    var newCodigo
+                    if(validarUsuario.stats[0] == '4'){
+                        newCodigo = Codigo(4, validarUsuario.id)
+                    }else{
+                        newCodigo = Codigo(3, validarUsuario.id)
+                    }
+                    let entradasSalvarStatus = 0
+                    while(entradasSalvarStatus < 3){
+                        try{
+                            EnviarEmailInicial(validarUsuario.email, validarUsuario.nome, newCodigo, 2)
+                            UserConfig.update(
+                                { endereco: req.ip, stats: newCodigo},
+                                { where: { id: validarUsuario.id} }
+                            )
+                            break;
+                        }catch(error){
+                            entradasSalvarStatus++
+                        }
+                    }
+                    if(entradasSalvarStatus === 3){
+                        return res.json({
+                            status: `1_BC#07203`,
+                            msg: `Erro de atualização, tente realizar o login novamente`,
+                        });
+                    }
+                    return res.json({
+                        status: '1',
+                        id: validarUsuario.id
+                    });
                 }
-                let entradasSalvarStatus = 0
-                while(entradasSalvarStatus < 3){
+                if(validarUsuario.stats[0] == '2'){
+                    console.log(`o ${sanitizedData.form.email} lembrou da senha`)
                     try{
-                        EnviarEmailInicial(validarUsuario.email, validarUsuario.nome, newCodigo, 2)
+                        EnviarEmailInicial(validarUsuario.email, validarUsuario.nome, '0000', 3)
                         UserConfig.update(
-                            { endereco: req.ip, stats: newCodigo},
+                            { stats: "0"},
                             { where: { id: validarUsuario.id} }
                         )
-                        break;
-                    }catch(error){
-                        entradasSalvarStatus++
+                    }catch{
+                        return res.json({
+                            status: `1_BC#07203`,
+                            msg: `Erro de atualização, tente realizar o login novamente`,
+                        });
                     }
                 }
-                if(entradasSalvarStatus === 3){
-                    return res.json({
-                        status: `1_BC#07203`,
-                        msg: `Erro de atualização, tente realizar o login novamente`,
-                    });
-                }
+                console.log(`o ${sanitizedData.form.email} está logado`)
+                var token = jwt.sign({id: validarUsuario.id, nome: validarUsuario.nome, plano: validarUsuario.plano}, "OD2DS8S21DSA4SD4SS3A")
                 return res.json({
-                    status: '1',
-                    id: validarUsuario.id
+                    status: `0`,
+                    msg: `sucesso`,
+                    token: token,
+                });
+            }else{
+                return res.json({
+                    status: `2_BL#07203`,
+                    msg: `Senha incorreta`,
                 });
             }
-            if(validarUsuario.stats[0] == '2'){
-                console.log(`o ${sanitizedData.form.email} lembrou da senha`)
-                try{
-                    EnviarEmailInicial(validarUsuario.email, validarUsuario.nome, '0000', 3)
-                    UserConfig.update(
-                        { stats: "0"},
-                        { where: { id: validarUsuario.id} }
-                    )
-                }catch{
-                    return res.json({
-                        status: `1_BC#07203`,
-                        msg: `Erro de atualização, tente realizar o login novamente`,
-                    });
-                }
-            }
-            console.log(`o ${sanitizedData.form.email} está logado`)
-            var token = jwt.sign({id: validarUsuario.id, nome: validarUsuario.nome, plano: validarUsuario.plano}, "OD2DS8S21DSA4SD4SS3A")
-            return res.json({
-                status: `0`,
-                msg: `sucesso`,
-                token: token,
-            });
-        }else{
-            return res.json({
-                status: `2_BL#07203`,
-                msg: `Senha incorreta`,
-            });
         }
+        return res.json({
+            status: `1_BL#21170`,
+            msg: `nenhum usuário encontrado!`,
+        });
+    }catch (error) {
+        return res.status(429).json({
+          msg: 'Você tentou realizar esta ação muitas vezes. Aguarde alguns minutos e tente novamente.',
+          status: '3_MD#92429',
+        });
     }
-    return res.json({
-        status: `1_BL#21170`,
-        msg: `nenhum usuário encontrado!`,
-    });
+});
+router.post('/validar', [body('id').trim().isNumeric().withMessage('Id errado'),body('form.a').trim().isLength({ max: 1 }).withMessage('tem muitos digitos!'),body('form.b').trim().isLength({ max: 1 }).withMessage('tem muitos digitos!'),body('form.c').trim().isLength({ max: 1 }).withMessage('tem muitos digitos!'),body('form.d').trim().isLength({ max: 1 }).withMessage('tem muitos digitos!'),body('form.e').trim().isLength({ max: 1 }).withMessage('tem muitos digitos!'),body('form.f').trim().isLength({ max: 1 }).withMessage('tem muitos digitos!')], async (req, res) => { 
+    try{
+        const clientIp = req.ip
+        const rateLimiterRes = await limiter.consume(clientIp);
+        const sanitizedData = {
+            form: {
+                a: DOMPurify.sanitize(req.body.form.a),
+                b: DOMPurify.sanitize(req.body.form.b),
+                c: DOMPurify.sanitize(req.body.form.c),
+                d: DOMPurify.sanitize(req.body.form.d),
+                e: DOMPurify.sanitize(req.body.form.e),
+                f: DOMPurify.sanitize(req.body.form.f),
+            },
+            id: DOMPurify.sanitize(req.body.id)
+        };
+        const validarUsuario = await UserConfig.findOne({
+            where: {
+                id: sanitizedData.id,
+            }
+        });
+        if(validarUsuario){
+            if(validarUsuario.stats[0] == 3 || validarUsuario.stats[0] == 4){
+                const codParaValidar = validarUsuario.stats.split('&')[2].toString().toUpperCase()
+                const codDigitado = sanitizedData.form.a+sanitizedData.form.b+sanitizedData.form.c+sanitizedData.form.d+sanitizedData.form.e+sanitizedData.form.f
+                if(codParaValidar == codDigitado.toString().toUpperCase()){
+                    try{
+                        UserConfig.update(
+                            { stats: `${validarUsuario.stats[0] == 3 ? '0' : '5'}` },
+                            { where: { id: validarUsuario.id} }
+                        )
+                        var token = jwt.sign({id: validarUsuario.id, nome: validarUsuario.nome, plano: validarUsuario.plano}, "OD2DS8S21DSA4SD4SS3A")
+                        console.log(`o ${sanitizedData.form.email} está validado`)
+                        return res.json({
+                            status: `0`,
+                            msg: `sucesso`,
+                            token: token
+                        });
+                    }catch(error){
+                        return res.json({
+                            status: `1_VF#12255`,
+                            msg: `refaça o login novamente, não conseguimos atualizar o nosso banco de dados`,
+                        });
+                    }
+                }
+                return res.json({
+                    id: validarUsuario.id,
+                    status: `1`,
+                    msg: `ops, codigo digitado invalido, tente verificar possiveis erros, ou refaça o login!`,
+                });
+            }
+        }
+        return res.json({
+            status: `1_BL#21170`,
+            msg: `nenhum usuário encontrado!`,
+        });
     }catch (error) {
         return res.status(429).json({
           msg: 'Você tentou realizar esta ação muitas vezes. Aguarde alguns minutos e tente novamente.',
